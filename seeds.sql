7a6c3bc8f0c2542f0e819756557ad6752d6ba6da
Lisa McDonald <nixxxxer@aol.com>
2020-09-21 12:10:36 -0400

    Updated seeds file

--- seeds.sql
+++ seeds.sql
@@ -1,39 +1,50 @@
 -- SQL QUERIES TO INITIALIZE THE DATA; INSERTS DATA IN THE DATABASE --
 USE EMPLOYEE_TRACKER;
 
-INSERT INTO department (name)
-VALUES ("Accounting");
-INSERT INTO department (name)
-VALUES ("Human Resources");
+
+-- inserts values into department table --
+INSERT INTO department (name) VALUES ("Accounting");
+INSERT INTO department (name) VALUES ("Human Resources");
+INSERT INTO department (name) VALUES ("Information Technology");
+INSERT INTO department (name) VALUES ("Legal");
+INSERT INTO department (name) VALUES ("Marketing");
 
 
-INSERT INTO role (title, salary, department_id)
-VALUES ("Lead Accountant", 100000, 1);
-INSERT INTO role (title, salary, department_id)
-VALUES ("Accountant", 75000, 1);
+-- inserts values into role table --
+INSERT INTO role (title, salary, department_id) VALUES ("Lead Accountant", 100000, 1);
+INSERT INTO role (title, salary, department_id) VALUES ("Accountant", 75000, 1);
 
-INSERT INTO role (title, salary, department_id)
-VALUES ("HR Manager", 80000, 2);
-INSERT INTO role (title, salary, department_id)
-VALUES ("HR Coordinator", 600000, 2);
+INSERT INTO role (title, salary, department_id) VALUES ("HR Manager", 80000, 2);
+INSERT INTO role (title, salary, department_id) VALUES ("HR Coordinator", 60000, 2);
+
+INSERT INTO role (title, salary, department_id) VALUES ("Lead Engingeer", 90000, 3);
+INSERT INTO role (title, salary, department_id) VALUES ("Software Engineer", 70000, 3);
+
+INSERT INTO role (title, salary, department_id) VALUES ("Legal Team Lead", 120000, 4);
+INSERT INTO role (title, salary, department_id) VALUES ("Lawyer", 100000, 4);
+
+INSERT INTO role (title, salary, department_id) VALUES ("Marketing Manager", 75000, 5);
+INSERT INTO role (title, salary, department_id) VALUES ("Marketer", 60000, 5);
 
 
-INSERT INTO employee (first_name, last_name, role_id)
-VALUES ("Mickey", "Mouse", 1);
-INSERT INTO employee (first_name, last_name, role_id)
-VALUES ("Donald", "Duck", 2);
-INSERT INTO employee (first_name, last_name, role_id)
-VALUES ("Roger", "Rabbit", 2);
+-- inserts values into employee table --
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Mickey", "Mouse", 1);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Donald", "Duck", 2);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Roger", "Rabbit", 2);
 
-INSERT INTO employee (first_name, last_name, role_id)
-VALUES ("Minnie", "Mouse", 3);
-INSERT INTO employee (first_name, last_name, role_id)
-VALUES ("Jessica", "Rabbit", 4);
-INSERT INTO employee (first_name, last_name, role_id)
-VALUES ("Daisy", "Duck", 4);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Minnie", "Mouse", 3);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Jessica", "Rabbit", 4);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Daisy", "Duck", 4);
 
--- ### Alternative way to insert more than one row
--- INSERT INTO employee (first_name, last_name)
--- VALUES ("Lisa", "Gautier"), ("Roger", "Rabbit"), ("Happy", "Madison");
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Bugs", "Bunny", 5);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Elmer", "Fudd", 6);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Daffy", "Duck", 6);
 
--- INSERT INTO employee (first_name, last_name) VALUES ("Luke", "Skywalker"), ("Han", "Solo"), ("Harry", "Potter"); --
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Luke", "Skywalker", 7);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Han", "Solo", 8);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Lando", "Calrissian", 8);
+
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Hermione", "Granger", 9);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Harry", "Potter", 10);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Ron", "Weasley", 10);
+
